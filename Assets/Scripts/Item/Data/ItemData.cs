f76b0e00da2bff0daf9c31a881d4c8596eda85aa
lsw7791 <lsw7791@naver.com>
2024-10-29 19:45:19 +0900

    Item

---
+++ Assets/Scripts/Item/Data/ItemData.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public enum ItemType
+{
+    Equipable,
+    Consumable
+}
+
+public enum ConsumableType
+{
+    Health,
+    Stamina
+}
+
+[System.Serializable]
+public class ItemDataConsumable
+{
+    public ConsumableType type;
+    public float value;
+}
+
+[CreateAssetMenu(fileName = "Item", menuName = "New Item")]
+public class ItemData : ScriptableObject
+{
+    [Header("Info")]
+    public string displayName;
+    public string description;
+    public ItemType type;
+    public Mesh icon;
+    public GameObject dropPrefab;
+
+    [Header("Stacking")]
+    public bool canStack;
+    public int maxStackAmount;
+
+    [Header("Consumable")]
+    public ItemDataConsumable[] consumables;
+
+    [Header("Equip")]
+    public GameObject equipPrefab;
+}
